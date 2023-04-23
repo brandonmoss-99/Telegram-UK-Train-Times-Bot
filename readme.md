b2067d5bb606021c72f270bb0ad685fea585dc3c
Brandon Moss <88378432+brandonmoss-99@users.noreply.github.com>
2023-04-23 23:44:34 +0100

    Containerise

--- readme.md
+++ readme.md
@@ -1,35 +1,16 @@
 # telegram-railTime-bot
 
-A telegram bot script written in Python for fetching and displaying UK train times and station notice boards
+## What does this do?
+This is a bot for the Telegram messaging service, with the purpose of fetching and displaying UK train times and station notice boards. It is intended to be run within a containerised solution.
 
-## Dependencies
- - [Python 3.8+](https://www.python.org/downloads/) - May work with earlier versions, but I haven't tested it on older than Python 3.8.5
+## Configuration
+### Environment variables
+The following environment variables are used by the bot, and should be passed into the container's environment when run:
+#### Required:
+  - `T_TOKEN` - The Telegram Bot API token to use. Should be a String, `"12345:AAAABBBBCCCCDDDD"`
+  - `RAIL_TOKEN` - The National Rail OpenLDBWS API token to use. Should be a String
 
-See requirements.txt for additional dependencies
+#### Optional:
+  - `NEXT_TRAINS_COUNT` - The max number of trains to fetch per update. Must be between 1 and 10. Defaults to 5.
+  - `CACHE_TIME` - The time in seconds to cache responses from the National Rail OpenLDBWS API. Defaults to 30.
 
-## Usage
-
-To run the script, call it, passing in your Telegram bot token and National Rail API token:
-
-```bash
-$ python3 src/bot.py [OPTIONS]...
-```
-
-See also `python3 src/bot.py --help`.
-
-### Options
-
-Required:
-
-`-t` or `--bottoken`: Telegram Bot token to use for connection to the API.
-
-`-r` or `--railtoken`: National Rail token to use for connection to the API.
-
-Optional:
-
-`-n` or `--next`: Max number of trains to fetch per update [1-10]
-
-`--help`: Display help
-
-## Compatibility
-I've been running this bot successfully on MacOS 10.14.6, and Ubuntu 20.04.1 LTS. I haven't tested its functionality on Windows
--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,4 @@
-import time, sys, getopt, requests, json, random, logging
+import time, sys, requests, json, random, logging, os
 from tMsgSender import tMsgSender
 from tMsgFetcher import tMsgFetcher
 from rMsgFetcher import rMsgFetcher
@@ -24,66 +24,78 @@ def getHelp():
 if __name__ == '__main__':
 	# Create the logger to use
 	logging.basicConfig(format='%(asctime)s %(levelname)s - %(message)s', level=logging.INFO)
-	
-	railtoken = None # National Rail OpenLDBWS token
-	numberNextTrains = 5 # fetch 5 next trains by default
-	staleThreshold = 20 # max age of data in seconds to fetch from dictionary instead of re-requesting from API (seconds)
-	logging.debug(f"Set initial default value for the number of trains to fetch to {numberNextTrains}\n and the max age of the cache to {staleThreshold} seconds")
-	argv = sys.argv[1:]
 
-	# try getting supported parameters and args from command line
-	try:
-		logging.debug("Trying to parse the given arguments")
-		opts, args = getopt.getopt(argv, "r:t:n:",
-			["railtoken=", "bottoken=", "next=", "help"])
-	except:
-		logging.error("Failed to parse the given arguments")
+	DEFAULT_NEXT_TRAINS = 5
+	DEFAULT_STALE_THRESHOLD = 30
+
+	# Values could be of type "None" or str
+	bottoken = os.environ.get("T_TOKEN") # Telegram bot API token
+	railtoken = os.environ.get("RAIL_TOKEN") # National Rail OpenLDBWS token
+	numberNextTrains = os.environ.get("NEXT_TRAINS_COUNT") # Max number of trains to fetch
+	staleThreshold = os.environ.get("CACHE_TIME") # Time to cache data before re-requesting from API (secs)
+
+	# Check the variables have been set
+	if bottoken != None:
+		logging.debug("Attempting to validate the provided Telegram bot token")
+		try:
+			# connect to Telegram API with their getMe test method for checking API works
+			testResponse = requests.get("https://api.telegram.org/bot%s/getMe" % (str(bottoken)))
+			# set the token to be used if we get a 2xx response code back
+			if testResponse.ok:
+				logging.info("Provided Telegram token was validated successfully")
+				bottoken = str(bottoken)
+			else:
+				logging.error("Telegram bot token validation failed")
+				getHelp()
+		except Exception as ex:
+			logging.error(f"There was an error trying to validate the Telegram bot token {ex}")
+			getHelp()
+	else:
+		logging.error("Telegram bot API token is missing")
+		getHelp()
+	
+
+	if railtoken == None:
+		logging.error("Rail API token is missing")
 		getHelp()
 
-	# assign variables based on command line parameters and args
-	for opt, arg in opts:
-		if opt in ['-t', '--bottoken']:
-			try:
-				logging.debug("Attempting to validate the provided Telegram bot token")
-				# connect to Telegram API with their getMe test method for checking API works
-				testResponse = requests.get("https://api.telegram.org/bot%s/getMe" % (str(arg)))
-				# set the token to be used if we get a 2xx response code back
-				if testResponse.ok:
-					logging.info("Provided Telegram token was validated successfully")
-					bottoken = str(arg)
-				else:
-					logging.error("Telegram bot token validation failed")
-					getHelp()
-			except Exception as ex:
-				logging.error(f"There was an error trying to validate the Telegram bot token {ex}")
-				getHelp()
 
-		if opt in ['-r', '--railtoken']:
-			railtoken = arg
+	if numberNextTrains != None:
+		try:
+			numberNextTrains = int(numberNextTrains)
+			# make sure the no. trains to fetch isn't abusing any OpenLDBWS limits
+			if numberNextTrains < 1:
+				numberNextTrains = 1
+				logging.info(f"Specified next trains value is below min threshold. Set to min of {numberNextTrains}")
+			elif numberNextTrains > 10:
+				numberNextTrains = 10
+				logging.info(f"Specified next trains value exceeds max threshold. Set to max of {numberNextTrains}")
+			else:
+				logging.info(f"Set number of next trains to {numberNextTrains}")
+		except:
+			numberNextTrains = DEFAULT_NEXT_TRAINS
+			logging.info(f"Set number of next trains to the default value of {numberNextTrains}")
+	else:
+		# Set a default value
+		numberNextTrains = DEFAULT_NEXT_TRAINS
+		logging.info(f"Set number of next trains to the default value of {numberNextTrains}")
+	
 
-		if opt in ['-n', '--next']:
-			try:
-				logging.debug("Attempting to set the max number of the next trains to fetch")
-				numberNextTrains = int(arg)
-				logging.info(f"Successfully set the max number of next trains to fetch to {numberNextTrains}")
-			except:
-				logging.warning(f"Failed to convert the given number of next trains to fetch. Using default of {numberNextTrains}")
-
-		if opt in ['--help']:
-			getHelp()
+	if staleThreshold != None:
+		try:
+			staleThreshold = int(staleThreshold)
+			logging.info(f"Set the cache time to {staleThreshold} seconds")
+		except:
+			staleThreshold = DEFAULT_STALE_THRESHOLD
+			logging.info(f"Set the cache time to the default {DEFAULT_STALE_THRESHOLD} seconds ")
+	else:
+		# Set a default value
+		staleThreshold = DEFAULT_STALE_THRESHOLD
+		logging.info(f"Set the cache time to the default {DEFAULT_STALE_THRESHOLD} seconds ")
 
 	logging.info(f"Initial configuration finished")
 
-	# make sure the no. trains to fetch isn't abusing any OpenLDBWS limits
-	if numberNextTrains < 1:
-		numberNextTrains = 1
-	elif numberNextTrains > 10:
-		numberNextTrains = 10
-
-	# make sure a token has been given before doing anything
-	logging.debug("Checking rail API token has been provided")
-	if railtoken is not None:
-		logging.debug("Rail API token has been provided")
+	if railtoken is not None and bottoken is not None:
 		msgOffset = 0
 		pollTimeout = 20
 
@@ -129,5 +141,3 @@ if __name__ == '__main__':
 				sleepTime = random.randint(20, 60)
 				logging.warning(f"Failed to fetch new messages from Telegram. Sleeping for {sleepTime} seconds")
 				time.sleep(sleepTime)
-	else:
-		logging.error("Rail API token wasn't given")
